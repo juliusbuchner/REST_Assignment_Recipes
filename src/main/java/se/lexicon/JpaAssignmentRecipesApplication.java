3568ccae21b8e065bbcfd3e9ad2685538ada9c50
Julius <juliusbuchner@gmail.com>
2022-09-16 14:37:11 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/JpaAssignmentRecipesApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpa_assignment_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAssignmentRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAssignmentRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/JpaAssignmentRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpa_assignment_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAssignmentRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
